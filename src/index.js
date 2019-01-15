ef0068c231f970cdd4c8950b6b6c924b0be26949
House <HOUSECH@KCO.AD>
2019-01-15 15:57:06 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,86 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let films = [
+    {
+        'title': 'Aguire: The Wrath of God',
+        'year': '1978'
+    },
+    {
+        'title': 'Fitzcarraldo',
+        'year': '1983'
+    },
+    {
+        'title': 'Grizzly Man',
+        'year': '2004'
+    },
+    {
+        'title': 'Rescue Dawn',
+        'year': '2006'
+    }
+]
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+class Hiring extends React.Component {
+    render() {
+        return (
+            <div>We are hiring! Welcome!</div>
+        )
+    }
+}
+
+class NotHiring extends React.Component {
+    render() {
+        return (
+            <div>We are not hiring! Go away!</div>
+        )
+    }
+}
+
+class Filmography extends React.Component {
+
+    state = { 
+        complete: true,
+        freeBookmark: true,
+        hiring: false
+    };
+
+    changeComplete = () => {
+        this.setState(prevState => ({
+            complete: !prevState.complete
+        }));
+    }
+    render() {
+        let { films } = this.props;
+        return (
+            <div>
+                {this.state.hiring ? <Hiring /> : <NotHiring />}
+                <h1>The filmography is {this.state.complete ? 'complete' : 'not complete'}</h1>
+                {films.map((film, i) => 
+                    <Film 
+                        key={i} 
+                        title={film.title} 
+                        year={film.year}
+                        freeBookmark={this.state.freeBookmark} />
+                )}
+                <button onClick={this.changeComplete}>Click</button>
+            </div>
+        )
+    }
+}
+
+class Film extends React.Component {
+    render() {
+        let { title, year, freeBookmark } = this.props;
+        return (
+            <li>
+                {title} ({year})
+                <p>Free Boomark Today: {freeBookmark ? 'Yes' : 'Nope'}</p>
+            </li>            
+        )
+    }
+}
+
+ReactDOM.render(
+    <Filmography films={films} />,
+    document.getElementById('root')
+);
